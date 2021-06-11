17233c803ec969c2bd4239635810e801650f8ad1
Cameron Salisbury <cam2772@gmail.com>
2021-06-11 14:41:53 +1200

    Made procedure and function declarations consistent

--- src/Interpreter/Core/Dec.hs
+++ src/Interpreter/Core/Dec.hs
@@ -39,18 +39,20 @@ evalDec (RecordDec i1 is _) w r u s = u (newEnv i1 (ERecord record)) s'
     record = Record record'
 evalDec (ProcDec i1 i2 _ c1) w r u s = u (newEnv i1 procd) s
   where
-    procd' c e = evalCom c1 (w ! 3) (updateEnv (newEnvMulti i2 e) r) c
+    procd' c es = evalCom c1 (w ! 3) (updateEnv (newEnvMulti i2 es) r) c
     procd = EProc procd' (length i2)
 evalDec (RecProcDec i1 i2 _ c1) w r u s = u (newEnv i1 procd) s
   where
-    procd = EProc (\c e -> evalCom c1 (w ! 3) (updateEnv (newEnvMulti (i1 : i2) (procd : e)) r) c) (length i2)
+    procd' c es = evalCom c1 (w ! 3) (updateEnv (newEnvMulti (i1 : i2) (procd : es)) r) c
+    procd = EProc procd' (length i2)
 evalDec (FuncDec i1 i2 _ _ e1) w r u s = u (newEnv i1 func) s
   where
-    func' k e = evalExp e1 (w ! 3) (updateEnv (newEnvMulti i2 e) r) k
+    func' k es = evalExp e1 (w ! 3) (updateEnv (newEnvMulti i2 es) r) k
     func = EFunc func' (length i2)
 evalDec (RecFuncDec i1 i2 _ _ e1) w r u s = u (newEnv i1 func) s
   where
-    func = EFunc (\k e -> evalExp e1 (w ! 3) (updateEnv (newEnvMulti (i1 : i2) (func : e)) r) k) (length i2)
+    func' k es = evalExp e1 (w ! 3) (updateEnv (newEnvMulti (i1 : i2) (func : es)) r) k
+    func = EFunc func' (length i2)
 evalDec (FileDec i1 i2 t1) w r u s = evalFileDec (FileDec i1 i2 t1) w r u s
 evalDec (ClassDec i1 scd1) w r u s = evalClassDec (ClassDec i1 scd1) w r u s
 evalDec (ChainDec d1 d2) w r u s = evalDec d1 (w ! 1) r (\r1 -> evalDec d2 (w ! 2) (updateEnv r1 r) (\r2 -> u (updateEnv r2 r1))) s
